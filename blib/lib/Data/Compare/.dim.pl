45ca53f06b149de7242cc63717a4b90d99bcc431
Shinichiro Sato <s2otsa59@gmail.com>
2012-10-31 09:48:08 +0000

    Import of SATOSIN/Data-Compare-Type-0.01 from CPAN.
    gitpan-cpan-distribution: Data-Compare-Type
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SATOSIN/Data-Compare-Type-0.01.tar.gz
    gitpan-cpan-author:       SATOSIN
    gitpan-cpan-maturity:     released

--- blib/lib/Data/Compare/.dim.pl
+++
@@ -1,7 +0,0 @@
-# This file is managed by /usr/local/bin/dim.
-{
-  dist => 'Data::Compare::Type',
-  distdir => 'Data-Compare-Type',
-  module => 'Data::Compare::Type',
-  template => 'Default'
-}
--- test.pl
+++
@@ -1,9 +0,0 @@
-use strict;
-use 5.14.0;
-
-use lib './lib';
-use Data::Compare::Type;
-
-my $validate = Data::Compare::Type->new();
-$validate->check([] , [{id =>"INT"}]);
-say $validate->has_error;
